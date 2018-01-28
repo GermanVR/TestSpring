ec7591122769fb10b5d29618764a1d6fb0d25982
German <german_1241@hotmail.com>
2018-01-28 01:22:08 -0600

    Prueba de subida

--- src/src/Domino.java
+++ src/src/Domino.java
@@ -9,7 +9,6 @@ public class Domino {
 			int fila; 
 			for (fila = (int) Math.sqrt(2 * num); fila >= 1; fila--) { //con la raiz podemos obtener el numero aproximado de bloques en primera instancia
 
-
 				subloque = (2 * num - fila * (fila - 1)) / (2 * fila);  //se evalua si el subloque completa una fila nueva
 				if (2 * num == fila * (2 * subloque + fila - 1)) { 
 					//comparamos si el valor de n es igual a una fila de a es 
